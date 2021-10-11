95d91f2d0c58f941e85dcd91412bc86cea2d20bd
dmitrykachala <kosas@mail.ru>
2021-10-11 16:45:10 +0300

    2. Анализ доступности сервера. [#859 #214537]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
@@ -29,25 +28,19 @@ public class Analizy {
                 status = false;
                 str = "";
             }
-            try (PrintWriter outFile = new PrintWriter(new FileOutputStream(target))) {
-                outFile.println(line);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter outFile = new PrintWriter(new FileOutputStream(target))) {
+            outFile.println(line);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
             Analizy test = new Analizy();
             test.unavailable("data/server.log", "data/target.txt");
             test.unavailable("data/server1.log", "data/target1.txt");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
