0731d54f465169c748aece84b02b27dbec1359f3
dmitryu.kachala <kosas@mail.ru>
2021-10-15 20:38:46 +0300

    5.2. Архивировать проект [#861 #215922]

--- src/main/java/ru/job4j/io/ArgsParse.java
+++ src/main/java/ru/job4j/io/ArgsParse.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,18 +11,22 @@ public class ArgsParse {
     private String exclude;
     private String targetFileName;
 
-    public void parse(String[] args) throws IOException, IllegalArgumentException {
+    public void parse(String[] args) throws FileNotFoundException, IllegalArgumentException {
         if (args.length != 3) {
             throw new IllegalArgumentException("Count of parameters are not enough."
                     + "Usage: java -jar pack.jar -d=FOLDER_FOR_ARCH "
                     + "-e=EXCLUDE_PATTERN -o=TARGET_FILE");
         }
         ArgsName arguments = ArgsName.of(args);
+        if (arguments.get("d").equals("") || arguments.get("e").equals("")
+                || arguments.get("o").equals("")) {
+            throw new IllegalArgumentException("One or more parameters are wrong.");
+        }
         dir = arguments.get("d");
         exclude = arguments.get("e");
         targetFileName = arguments.get("o");
         if (!Files.exists(Paths.get(dir))) {
-            throw new IOException("There is no such directory.");
+            throw new FileNotFoundException("There is no such directory.");
         }
     }
 
