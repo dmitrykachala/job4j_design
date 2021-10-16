0f8c3dd2e810d48cc66f382cd0b7eb0ebbe948a9
dmitryu.kachala <kosas@mail.ru>
2021-10-16 10:43:06 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -10,24 +10,40 @@ import java.util.StringJoiner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        validate(argsName);
+
+        String path = argsName.get("path");
+        String delimiter = argsName.get("delimiter");
+        String out = argsName.get("out");
+        String filter = argsName.get("filter");
+        String[] filters = filter.split(",");
+
+        StringJoiner joiner = fileScan(path, filters, delimiter);
+
+        if (out.equals("stdout")) {
+            stdout(joiner);
+        } else {
+        outToFile(joiner, out);
+        }
+    }
+
+    private static void validate(ArgsName argsName) throws Exception {
         if (argsName.getSize() != 4) {
             throw new IllegalArgumentException("Count of parameters are not enough."
                     + "Usage: java -jar pack.jar -d=FOLDER_FOR_ARCH "
                     + "-e=EXCLUDE_PATTERN -o=TARGET_FILE");
         }
         String path = argsName.get("path");
-        String delimiter = argsName.get("delimiter");
-        String out = argsName.get("out");
-        String filter = argsName.get("filter");
-        String[] filters = filter.split(",");
-        ArrayList<Integer> fl = new ArrayList<>();
-        StringJoiner joiner = new StringJoiner(System.lineSeparator());
-        String str = "";
-
         if (!Paths.get(path).toFile().exists()) {
             throw new FileNotFoundException();
         }
+    }
 
+    private static StringJoiner fileScan(String path, String[] filters,
+                                         String delimiter) throws Exception {
+        ArrayList<Integer> fl = new ArrayList<>();
+        StringJoiner joiner = new StringJoiner(System.lineSeparator());
+        String str = "";
         Scanner scanner = new Scanner(new File(path));
         scanner.useDelimiter(System.lineSeparator());
         String[] columns = scanner.next().split(delimiter);
@@ -52,11 +68,19 @@ public class CSVReader {
             str = "";
         }
         scanner.close();
+        return joiner;
+    }
+
+    private static void outToFile(StringJoiner joiner, String out) {
         try (PrintWriter outFile = new PrintWriter(new FileOutputStream(out))) {
             outFile.println(joiner);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void stdout(StringJoiner joiner) {
+        System.out.println(joiner);
     }
 
     public static void main(String[] args) throws Exception {
