e0fa827751db7f2cd2eae60daeebf7267c343b5e
dmitryu.kachala <kosas@mail.ru>
2021-10-17 17:54:47 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Kadavr";
+        byte age = 33;
+        short pocketMoney = 12543;
+        char sex = 'M';
+        int savings = 874859;
+        long antCount = 94596946947L;
+        float height = 1.66F;
+        double weight = 77.7777D;
+        boolean isMarried = false;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.error("Sex : {}, marital status : {}", sex, isMarried);
+        LOG.warn("Height : {}, weight : {}", height, weight);
+        LOG.info("Savings : {}, pocket money: {}", savings, pocketMoney);
+        LOG.info("Ants number : {}", antCount);
     }
 }
