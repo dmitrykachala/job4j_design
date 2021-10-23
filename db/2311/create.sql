4cdd17dd2574056f114bb694e8debd1c6fb60f19
dmitryu.kachala <kosas@mail.ru>
2021-10-23 21:23:08 +0300

    1. Схема прав пользователи и роли. [#1093]

--- db/2311/create.sql
+++ db/2311/create.sql
@@ -1,23 +1,33 @@
+create table role (
+    id serial primary key,
+    name text
+);
+
+create table rules (
+    id serial primary key,
+    name text
+);
+
+create table state (
+    id serial primary key,
+    name text
+);
+
+create table category (
+    id serial primary key,
+    name text
+);
+
 create table user (
     id serial primary key,
     name text,
     role_id int references role(id)
 );
 
-create table role (
-    id serial primary key,
-    name text
-);
-
 create table role_rules (
     id serial primary key,
     role_id references role(id),
     rules_id references rules(id)
-);
-
-create table rules (
-    id serial primary key,
-    name text
 );
 
 create table item (
@@ -38,14 +48,4 @@ create table attachs(
     id serial primary key,
     name text,
     item_id references item(id)
-);
-
-create table category (
-    id serial primary key,
-    name text
-);
-
-create table state (
-    id serial primary key,
-    name text
 );
--- db/2311/insert.sql
+++ db/2311/insert.sql
@@ -7,3 +7,4 @@ insert into user(name, role_id) VALUES ('Ivan', 1);
 insert into role_rules(role_id, rules_id) VALUES (1, 1);
 insert into item(name, user_id, category_id, state_id) VALUES ('install', 1, 1, 1);
 insert into comments(name, item_id) VALUES ('as soon as possible', 1);
+insert into attachs(name, item_id) VALUES ('photo', 1);
