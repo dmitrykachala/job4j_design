e4bfe8e8dea027dd97d61310855204645db8fb00
dmitryu.kachala <kosas@mail.ru>
2021-10-25 10:58:58 +0300

    3. Фильтры [#1732 #220228]

--- db/2323/products.sql
+++ db/2323/products.sql
@@ -21,22 +21,22 @@ insert into product(name, type_id, expired_date, price) values ('сырок', 2,
 ('батон нарезной', 6, '2021-10-28', 35), ('яблоки', 8, '2021-11-27', 79);
 select * from product;
 
-select t.name as type, p.name, p.price from product as p join type as t on t.name = 'сыр' and p.type_id = t.id;
+select t.name as type, p.name, p.price from product as p, type as t where t.name = 'сыр' and p.type_id = t.id;
 
 select * from product where name like '%мороженое%';
 
 insert into product(name, type_id, expired_date, price) values ('молоко', 2, '2021-10-20', 67.6);
 select * from product where expired_date < current_date;
 
-select max(price) from product;
+select * from product as p where p.price = (select max(price) from product);
 
-select t.name, count(p.type_id) as count from product as p join type as t on p.type_id = t.id group by t.name;
+select t.name, count(p.type_id) as count from product as p, type as t where p.type_id = t.id group by t.name;
 
-select t.id, p.name, p.price from product as p join type as t on p.type_id = t.id and (t.name ='сыр' 
+select t.id, p.name, p.price from product as p, type as t where p.type_id = t.id and (t.name ='сыр' 
 or t.name ='молоко') order by t.id;
 
-select t.id, t.name, count(p.type_id) as count from product as p join type as t on p.type_id = t.id 
+select t.id, t.name, count(p.type_id) as count from product as p, type as t where p.type_id = t.id 
 group by t.id having count(p.type_id) < 10 order by t.id;
 
-select t.name as type, p.name, p.price from product as p join type as t on p.type_id = t.id 
-group by t.name, p.name, p.price order by t.name;
+select t.name as type, p.name, p.price from product as p, type as t where p.type_id = t.id 
+order by t.name;
