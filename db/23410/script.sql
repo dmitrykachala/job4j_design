feba3b9e681b37244b308d6d9ab9479b8c6213fc
dmitrykachala <kosas@mail.ru>
2022-12-13 11:01:05 +0300

    10. Операторы множества в SQL [#504896]

--- db/23410/script.sql
+++ db/23410/script.sql
@@ -32,6 +32,12 @@ select title from book
 except
 select name from movie;
 
+select title from book
+except
 select name from movie
 union
-select title from book;
+(
+    select name from movie
+    except
+    select title from book
+);
